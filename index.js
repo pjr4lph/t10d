03d79450bf4df17d26a6e478ca0cb17d8fc1c89b
ralph salazar <ralph.slzr@gmail.com>
2018-10-14 21:39:18 -0400

    upload working project

---
+++ index.js
@@ -0,0 +1,84 @@
+'use strict'
+
+module.exports = downloadPackages;
+
+const cheerio = require('cheerio');
+const request = require('request');
+const download = require('download-tarball');
+const getNpmTarballUrl = require('get-npm-tarball-url');
+const exec = require('child_process').exec;
+
+// array where package information will be held as objects
+const data = [];
+
+// this function scrapes the top ten most depended dependencies from the npmjs page
+function scrapePackages (count, callback) {
+  return new Promise((resolve, reject) => {
+    request('https://www.npmjs.com/browse/depended', (err, response, html) => {
+      if (err) return reject('error occurred');
+      let $ = cheerio.load(html);
+      // scrape for the name and rank of dependencies
+      $('main').find('div > div > section > div > div > a').each((idx, el) => {
+        if (data.length <= 9) {
+          const currPackage = {
+            rank: idx+1,
+            name: $(el).text()
+          }
+          // push the created object of each package into the data array
+          data.push(currPackage);
+        }
+        });
+        // now grab the version for each of the packages and save in the package object
+        $('main').find('div > div > section > div > div > span').each((idx, el) => {
+          if (idx <= 9) {
+            const spanText = $(el).text();
+            const currVersion = spanText.split(' ')[1];
+            data[idx]['version'] = currVersion;
+          }
+      });
+      resolve(data);
+    });
+  });
+}
+
+// this function downloads the tarball for each package and extracts them into the package directory
+function handleTarball(pkgObj) {
+  const currName = pkgObj['name'];
+  const currVersion = pkgObj['version']
+  const url = getNpmTarballUrl.default(currName, currVersion);
+
+  download({
+   url: url,
+   dir: `./packages/${currName}`
+  }).then(() => {
+   // this executable moves package files up one level directly into the package folder
+   exec(`mv packages/${currName}/package/* .* packages/${currName}/`);
+   console.log(`${currName} is now downloaded and extracted!`);
+  }).catch(err => {
+   console.log('the package could not be downloaded properly');
+   console.log(err);
+  });
+}
+
+// this function implements the helper functions
+async function downloadPackages(dataList) {
+  let list;
+  try {
+    list = await scrapePackages(dataList);
+  } catch(err) {
+    console.log('thrown value: ', err);
+  }
+// iterate through the list to download and extract the tarballs to the packages directory
+  for (const pkg of list) {
+    try {
+      const currPkg = await handleTarball(pkg);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  return list;
+}
+
+// invoke downloadPackages to download and extract package tarballs
+downloadPackages(data);
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Node.js Homework
+
+## Project
+
+1. Get the 10 [most depended on packages](https://www.npmjs.com/browse/depended) from npm.
+2. For each package, download the latest tarball from the npm registry.
+3. Extract the tarball into `./packages/${pkg.name}`, e.g. `./packages/lodash`.
+
+## To Start
+
+Download repository to local machine
+
+Change into the repository's directory
+
+To execute run ``node index.js``
+
+## Testing
+
+We've created a failing `npm test` command for you. You can add additional tests if you'd like and even bring in a tool other than [`tape`](https://github.com/substack/tape) as long as these initial tests remain unchanged and `npm test` sets correct exit codes.
+
+Passing tests don't guarantee that your solution is perfect but a failing test definitely indicates a problem.
+# t10d
+# t10d
